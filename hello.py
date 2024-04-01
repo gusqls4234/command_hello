9ce943c78ea99ffcf782f4b4c10304bacf27d521
unknown <gusqls4234@daegu.ac.kr>
2024-04-01 11:45:40 +0900

    cobflict resolved GitHib

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 // For command line git tutorial
-// Local repository modification
-// Github modification
+// First: GitHub modification
+// Second: Local repository modification
 
+>>>>>>> origin/master
 print("Hello.Wold")
 print("Tell your world")
 print("Tell his world")
